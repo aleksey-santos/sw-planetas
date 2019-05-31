6d4feee37c928aa5a642c093dfaeba8b90b59561
= <alekseys@gmail.com>
2019-05-31 00:38:34 -0300

    Inicial

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
-    <h1 id="titulo"></h1>
-    <Planeta :planeta="planeta"/>
+    <h1 id="titulo">Star Wars Planets</h1>
+    <transition name="zoom">
+    <Planeta :planeta="planeta" v-show="!loading"/>
+    </transition>
     <div id="container-botao">
-      <button @click="planetaAleatorio()">Blow this up</button>
+      <button id="btn-next" @click="planetaAleatorio()">Next</button>
     </div>
   </div>
 </template>
@@ -19,7 +21,13 @@ export default {
   },
   data(){
     return {
-      planeta: null
+      planeta: {
+        population:'',
+        climate:'',
+        terrain:'',
+        films:[]
+      },
+      loading:true
     }
   },
   methods:{
@@ -28,6 +36,7 @@ export default {
       return Math.floor(Math.random() * (max - 1 + 1)) + 1;
     },
     async carregarPlaneta(idPlaneta){
+      this.loading = true;
       try{
         let resposta = await axios.get(`https://swapi.co/api/planets/${idPlaneta}`);
         console.log(resposta);
@@ -38,6 +47,7 @@ export default {
         console.log(error);
         alert('Erro ao buscar informações de planeta!');
       }
+      this.loading = false;
     },
     async planetaAleatorio(){
       try{
@@ -62,5 +72,68 @@ export default {
 </script>
 
 <style>
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+}
 
+@font-face {
+    font-family: StarJedi;
+    src: url(assets/fonts/Starjedi.ttf);
+ }
+@font-face {
+    font-family: StarJediHollow;
+    src: url(assets/fonts/Starjhol.ttf);
+ }
+
+#app{
+  font-family: StarJedi,'sans-serif';
+  height:100vh;
+  background-color:#222;
+  color:yellow;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows:1fr 500px 1fr;
+  align-items: center;
+  justify-content: center;
+}
+#titulo{
+  font-size: 3em;
+  text-align: center;
+  font-family: StarJediHollow,sans-serif;
+}
+#container-botao{
+  width: 400px;
+      margin:auto;
+
+}
+#btn-next{
+  outline: none;
+  border:1px solid transparent;
+  width:100%;
+  padding:10px 10px;
+  font-size: 1.2em;
+  background-color:yellow;
+  cursor: pointer;
+  font-family: StarJedi,sans-serif;
+  color: black;
+  transition: all 0.3s ease-in-out;
+}
+#btn-next:hover{
+  color:yellow;
+  background-color: black;
+}
 </style>
--- src/components/Planeta.vue
+++ src/components/Planeta.vue
@@ -4,9 +4,9 @@
           {{ planeta.name }}
       </div>
       <div class="informacoes-planeta">
-          <div class="info-planeta">Population: {{ planeta.population }}</div>
-          <div class="info-planeta">Climate: {{ planeta.climate}}</div>
-          <div class="info-planeta">Terrain: {{ planeta.terrain }}</div>
+          <div class="info-planeta"><span class="titulo-info">Population:</span> {{ planeta.population }}</div>
+          <div class="info-planeta"><span class="titulo-info">Climate:</span> {{ planeta.climate}}</div>
+          <div class="info-planeta"><span class="titulo-info">Terrain:</span> {{ planeta.terrain }}</div>
       </div>
       <div class="filmes-planeta" v-if="qtdFilms > 0">
           Featured in {{ qtdFilms }} film{{ qtdFilms>1?'s':'' }}
@@ -32,5 +32,31 @@ export default {
 </script>
 
 <style>
+.planeta{
+    min-width:400px;
+    margin:auto;
+    background-color:white;
+    color: black;
+    text-align: center;
+    box-shadow: 0 3px 6px rgba(255,255,255,0.16), 0 3px 6px rgba(255,255,255,0.23);
+}
+.informacoes-planeta{
+    padding:20px 20px;
+}
+
+.info-planeta{
+    padding:10px;
+}
+.titulo-planeta{
+    color:yellow;
+    background-color: black;
+    text-align: center;
+    font-size: 2em;
+    padding:10px;
+    font-family: StarJediHollow,sans-serif;
+}
+.filmes-planeta{
+    padding:10px;
+}
 
 </style>
